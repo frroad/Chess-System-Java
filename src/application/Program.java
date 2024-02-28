9070cf09879c841c4677970a25abf5a9310c419e
frroad <frramos@outlook.com.br>
2024-02-28 13:38:02 -0300

    Classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			System.out.println("Hello world");
+			Position pos = new Position(3, 5);
+			System.out.println(pos);
 	}
 
 }
