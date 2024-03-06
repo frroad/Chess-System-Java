d5bbd44812d255d69f7f677f78d7caa58b536bd6
frroad <frramos@outlook.com.br>
2024-03-05 23:33:59 -0300

    Criação de posicoes e excessões do tabuleiro

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -55,7 +55,7 @@ public class Board {
 		return pieces [position.getRow()][position.getColumn()];
 	}
 	
-	public void placePeace(Piece piece, Position position) {
+	public void placePiece(Piece piece, Position position) {
 		if(thereIsAPiece(position)) {
 			throw new BoardException("There is already a piece on position");
 		}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,9 +1,7 @@
 package chess;
 
 import boardGame.Board;
-import boardGame.Position;
 import chess.pieces.King;
-import chess.pieces.Rook;
 
 public class ChessMatch {
 	private Board board;
@@ -23,10 +21,13 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePeace(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePeace(new King(board, Color.BLACK),new Position(0, 4));
-		board.placePeace(new King(board, Color.WHITE),new Position(7, 4));
+		placeNewPiece('a', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
 
 }
