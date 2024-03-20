489e79faffaf8f37375bc5d1121953df4d890f06
frroad <frramos@outlook.com.br>
2024-03-19 23:05:05 -0300

    Criacao de troca de jogador por turno

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,8 +18,8 @@ public class Program {
 
 		while (true) {
 			try {
-				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
+				UI.clearScreen(); 
+				UI.printMatch(chessMatch);
 				System.out.println();
 				System.out.println("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,11 +7,23 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn =1;
+		currentPlayer= Color.WHITE; 
 		initialSetup();
+	}
+	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public Color getCurrentPlayer() {
+		return currentPlayer;
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -36,6 +48,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source,target);
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 		return (ChessPiece)capturedPiece;	
 	}
 	
@@ -50,6 +63,10 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece  on source position"); 
 		}
+		if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("the chosen piece is not yours");
+		}
+			
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for chosen piece");
 			
@@ -65,6 +82,12 @@ public class ChessMatch {
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
+	
+	private void nextTurn() {
+		turn++;
+		currentPlayer = ( currentPlayer == Color.WHITE)? Color.BLACK : Color.WHITE;
+	}
+	
 
 	private void initialSetup() {
 		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
